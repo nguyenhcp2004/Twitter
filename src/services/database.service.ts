02dd7de66dc93cf635dd0592a610431ba3b6eaa3
Huynh Nguyen <yugivip277@gmail.com>
2024-05-09 17:08:02 +0700

    Tạo tweet

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -4,6 +4,7 @@ import User from '~/models/schemas/User.schema'
 import RefreshToken from '~/models/schemas/RefreshToken.schema'
 import Follower from '~/models/schemas/Follower.schema'
 import VideoStatus from '~/models/schemas/VideoStatus.schema'
+import Tweet from '~/models/schemas/Tweet.schema'
 dotenv.config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@project-be.pmqin6l.mongodb.net/?retryWrites=true&w=majority&appName=Project-be`
 
@@ -55,6 +56,10 @@ class DatabaseService {
     if (!exists) {
       this.followers.createIndex({ user_id: 1, followed_user_id: 1 })
     }
+  }
+
+  get tweets(): Collection<Tweet> {
+    return this.db.collection(process.env.DB_TWEETS_COLLECTION as string)
   }
 
   get users(): Collection<User> {
